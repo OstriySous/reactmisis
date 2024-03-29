3bd7df6c83df59915506dbc0df14f2ed7ac05dd7
tlgn <sendmework@ya.ru>
2024-03-29 15:51:46 +0300

    img add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 // Footer.js
 import React from 'react';
-import './Footer.css'; // Импорт стилей CSS для Footer
+import './Footer.css';
+import logo from './misis_logo_white_rus.svg'; // Импорт изображения
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
+                <img src={logo} alt="Логотип МИСиС" className="footer-logo" />
                 <p>Университет МИСиС</p>
                 <p>Контакты: info@misis.ru</p>
                 <p>&copy; {new Date().getFullYear()} Университет МИСиС. Все права защищены.</p>
